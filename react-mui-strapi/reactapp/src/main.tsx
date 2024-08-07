ceea22947bfbed9a3615fdfd21abd4c93de3f722
Faisol <faisol.wr@gmail.com>
2024-08-07 23:22:07 +0700

    [Setup] theme

--- react-mui-strapi/reactapp/src/main.tsx
+++ react-mui-strapi/reactapp/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from '@mui/material'
+import theme from './config/theme'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
