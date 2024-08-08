546db289bd0caf46225dd5ca11f9e22f228ee4fd
Faisol <faisol.wr@gmail.com>
2024-08-08 22:27:45 +0700

    [Update] add route

--- react-mui-strapi/reactapp/src/App.tsx
+++ react-mui-strapi/reactapp/src/App.tsx
@@ -3,6 +3,9 @@ import AuthLayout from "./layouts/AuthLayout"
 import Login from "./pages/Login"
 import BackendLayout from "./layouts/BackendLayout"
 import Dashboard from "./pages/Dashboard"
+import Product from "./pages/Product"
+import Report from "./pages/Report"
+import Setting from "./pages/Setting"
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         </Route>
         <Route element={<BackendLayout />}>
           <Route path="/backend/dashboard" element={<Dashboard />} />
+          <Route path="/backend/product" element={<Product />} />
+          <Route path="/backend/report" element={<Report />} />
+          <Route path="/backend/setting" element={<Setting />} />
         </Route>
       </Routes>
     </BrowserRouter>
