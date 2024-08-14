900456924bf4305f38875a6d4833eb5ee89570b3
Faisol <faisol.wr@gmail.com>
2024-08-14 22:11:11 +0700

    [Update] topbar add toggle

--- react-mui-strapi/reactapp/src/components/AppHeader.tsx
+++ react-mui-strapi/reactapp/src/components/AppHeader.tsx
@@ -1,12 +1,28 @@
-import { AppBar, Box, IconButton, Toolbar } from "@mui/material"
-import MenuIcon from '@mui/icons-material/Menu'
-import SettingsIcon from '@mui/icons-material/Settings'
-import LogoutIcon from '@mui/icons-material/Logout'
+import { AppBar, Avatar, Box, IconButton, Menu, MenuItem, Toolbar } from "@mui/material"
+import MenuIcon from "@mui/icons-material/Menu"
+import { Logout, Money, Settings, Person, LanguageOutlined, NotificationsOutlined } from "@mui/icons-material"
 import { useProSidebar } from "react-pro-sidebar"
+import { useState } from "react"
 
 const AppHeader = () => {
 
+    // useProSidebar hook to control the sidebar
     const { collapseSidebar, toggleSidebar, broken } = useProSidebar()
+
+    const [anchorEl, setAnchorEl] = useState(null);
+
+    const handleMenuOpen = (event: any) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleLogout = () => {
+        // Perform logout action
+        // Example: clear authentication token, redirect to login page, etc.
+    };
 
     return (
         <AppBar position="sticky" sx={styles.appBar}>
@@ -21,12 +37,40 @@ const AppHeader = () => {
                     sx={styles.appLogo}
                     src="/assets/logo_dash.avif" />
                 <Box sx={{ flexGrow: 1 }} />
-                <IconButton title="Settings" color="secondary">
-                    <SettingsIcon />
+                <IconButton title="Notifications" color="secondary">
+                    <NotificationsOutlined />
                 </IconButton>
-                <IconButton title="Sign Out" color="secondary">
-                    <LogoutIcon />
+                <IconButton title="Language" color="secondary">
+                    <LanguageOutlined />
                 </IconButton>
+                <IconButton
+                    size="large"
+                    aria-controls="menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                    onClick={handleMenuOpen}
+                >
+                    <Avatar src="/assets/profile.webp" alt="Avatar" sx={{ width: 28, height: 28 }} />
+                </IconButton>
+                <Menu
+                    id="menu"
+                    anchorEl={anchorEl}
+                    open={Boolean(anchorEl)}
+                    onClose={handleMenuClose}
+                >
+                    <MenuItem onClick={handleMenuClose} sx={{ px: '30px' }}>
+                        <Person sx={{ fontSize: '16px', mr: '20px' }} /> Profile
+                    </MenuItem>
+                    <MenuItem onClick={handleMenuClose} sx={{ px: '30px' }}>
+                        <Settings sx={{ fontSize: '16px', mr: '20px' }} /> Settings
+                    </MenuItem>
+                    <MenuItem onClick={handleMenuClose} sx={{ px: '30px' }}>
+                        <Money sx={{ fontSize: '16px', mr: '20px' }} /> Billing
+                    </MenuItem>
+                    <MenuItem onClick={handleLogout} sx={{ px: '30px' }}>
+                        <Logout sx={{ fontSize: '16px', mr: '20px' }} /> Logout
+                    </MenuItem>
+                </Menu>
             </Toolbar>
         </AppBar>
     )
